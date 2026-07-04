fcc23ab9ac833fec8fd0ba529ba63a75de6835c7
repolym <repolym@users.noreply.github.com>
2026-07-04 08:22:47 +0000

    deploy: 3823dfb8f64b0bcca184d9954ed4de3d65feb237

--- assets/flame-CRs7Z-qV.js
+++ assets/flame-CRs7Z-qV.js
@@ -1,4 +1,4 @@
-import{c}from"./index-BHOEcvih.js";/**
+import{g as c}from"./index-DjP5jors.js";/**
  * @license lucide-react v1.23.0 - ISC
  *
  * This source code is licensed under the ISC license.
